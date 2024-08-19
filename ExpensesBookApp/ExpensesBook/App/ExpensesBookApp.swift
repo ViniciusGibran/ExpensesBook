ca9c5fd3a5d712179813d7f57d46e67421115e3a
Vinicius Gibran <v-vinicius.gibran@Bryj.ai>
2024-08-19 14:49:45 +0100

    Initial project setup.

---
+++ ExpensesBookApp/ExpensesBook/App/ExpensesBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesBookApp.swift
+//  ExpensesBook
+//
+//  Created by Vinicius Gibran on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
