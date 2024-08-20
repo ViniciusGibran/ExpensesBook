925d2cdf0b224dff67aff27cdc0ab9f991e173ce
Vinicius Gibran <v-vinicius.gibran@Bryj.ai>
2024-08-20 23:57:56 +0100

    Modify App @main

--- ExpensesBookApp/ExpensesBook/App/ExpensesBookApp.swift
+++ ExpensesBookApp/ExpensesBook/App/ExpensesBookApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ExpensesBookApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ExpenseHistoryView()
         }
     }
 }
---
+++ ExpensesBookApp/ExpensesBook/Modules/Expense/ExpenseHistory/ExpenseHistoryView.swift
@@ -0,0 +1,8 @@
+//
+//  ExpenseHistoryView.swift
+//  ExpensesBook
+//
+//  Created by Vinicius Gibran on 20/08/2024.
+//
+
+import Foundation
