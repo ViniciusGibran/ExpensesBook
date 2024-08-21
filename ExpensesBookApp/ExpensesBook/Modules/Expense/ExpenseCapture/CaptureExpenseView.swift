6b18c19ee776f9ed5b324ae410d594f6b777c8b7
Vinicius Gibran <v-vinicius.gibran@Bryj.ai>
2024-08-21 17:54:07 +0100

    Add CaptureExpenseView + DocumentCameraView initial implementation

--- ExpensesBookApp/ExpensesBook/Modules/Expense/ExpenseCapture/CaptureExpenseView.swift
+++ ExpensesBookApp/ExpensesBook/Modules/Expense/ExpenseCapture/CaptureExpenseView.swift
@@ -5,4 +5,20 @@
 //  Created by Vinicius Gibran on 21/08/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct CaptureExpenseView: View {
+    @EnvironmentObject var navigationState: NavigationState
+    @State private var scannedImage: UIImage?
+    
+    var body: some View {
+        DocumentCameraView(scannedImage: $scannedImage)
+            .onDisappear {
+                if let image = scannedImage {
+                    navigationState.navigationTrigger.send(.showDetail(image))
+                } else {
+                    navigationState.navigationTrigger.send(.backToHistory)
+                }
+            }
+    }
+}
--- ExpensesBookApp/ExpensesBook/Modules/Expense/ExpenseCapture/DocumentCameraView.swift
+++ ExpensesBookApp/ExpensesBook/Modules/Expense/ExpenseCapture/DocumentCameraView.swift
@@ -5,4 +5,46 @@
 //  Created by Vinicius Gibran on 21/08/2024.
 //
 
-import Foundation
+import SwiftUI
+import VisionKit
+
+struct DocumentCameraView: UIViewControllerRepresentable {
+    @Binding var scannedImage: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    
+    func makeUIViewController(context: Context) -> VNDocumentCameraViewController {
+        let viewController = VNDocumentCameraViewController()
+        viewController.delegate = context.coordinator
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
+        let parent: DocumentCameraView
+        
+        init(_ parent: DocumentCameraView) {
+            self.parent = parent
+        }
+        
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+            if scan.pageCount > 0 {
+                parent.scannedImage = scan.imageOfPage(at: 0)
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+            // HERE TODO: handle error (show an alert)
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
