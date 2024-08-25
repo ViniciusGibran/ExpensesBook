cfc967aa9a7c7fca3444300cb37d1175d2f534ed
Vinicius Gibran <v-vinicius.gibran@Bryj.ai>
2024-08-25 14:05:37 +0100

    Add Color extension with additional hex handling

--- ExpensesBookApp/ExpensesBook/Common/Utilities/Color+Extension.swift
+++ ExpensesBookApp/ExpensesBook/Common/Utilities/Color+Extension.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color {
+    init?(hex: String) {
+        let r, g, b: Double
+        var hexColor = hex
+        
+        if hex.hasPrefix("#") { hexColor = String(hex.dropFirst()) }
+        
+        if hexColor.count == 6, let intCode = Int(hexColor, radix: 16) {
+            r = Double((intCode >> 16) & 0xFF) / 255.0
+            g = Double((intCode >> 8) & 0xFF) / 255.0
+            b = Double(intCode & 0xFF) / 255.0
+            self.init(red: r, green: g, blue: b)
+        } else {
+            return nil
+        }
+    }
+    
+    func toHexString() -> String {
+        let components = self.cgColor?.components ?? [0, 0, 0, 1]
+        let r = components[0]
+        let g = components[1]
+        let b = components[2]
+        return String(format: "#%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255))
+    }
+}
